732eb96e6cc4eb0c28e803444b25528df1e5e304
Ak-Yadav3122 <aichchhik12@gmail.com>
2025-01-28 11:43:25 +0530

    Set up TypeScript project and basic configuration

---
+++ index.js
@@ -0,0 +1,6 @@
+// let x = 1 // identify the type of x as number by using type inferencing
+// let x:number = 1 // here we explictely give the type of x as number
+// console.log(x);
+// By using interface we can define the type of the object
+var message = "Hello, TypeScript!";
+console.log(message);
---
+++ index.ts
@@ -0,0 +1,9 @@
+// let x = 1 // identify the type of x as number by using type inferencing
+// let x:number = 1 // here we explictely give the type of x as number
+
+// console.log(x);
+
+ // By using interface we can define the type of the object
+ 
+ const message: string = "Hello, TypeScript!";
+ console.log(message);
