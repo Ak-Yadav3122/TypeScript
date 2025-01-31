3cb7cc98636038818f292448440d35793bcee6a3
Ak-Yadav3122 <aichchhik12@gmail.com>
2025-01-31 15:10:27 +0530

    declaration of function Or Named Funnction

---
+++ functions/functionDeclaration.ts
@@ -0,0 +1,13 @@
+function add(a: number, b: number): number {
+  return a + b;
+}
+console.log(add(2, 4));
+
+// a: number, b: number → Explicitly type the parameters.
+// : number → The function returns a number.
+
+function Sub(a: number, b: number): number {
+  return a - b;
+}
+
+console.log(Sub(7, 3));
