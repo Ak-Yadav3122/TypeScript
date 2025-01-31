6989e1b7fb02f0123008a1a97d32b7cb49a5bd44
Ak-Yadav3122 <aichchhik12@gmail.com>
2025-01-31 15:17:23 +0530

    use function as a expression

---
+++ functions/functionAsExpression.ts
@@ -0,0 +1,30 @@
+//Function can also be stored in the variables.
+
+let addNumber = function (a: number, b: number): number {
+  return a + b;
+};
+console.log(addNumber(16, 4));
+
+let subNumber = function (a: number, b: number): number {
+  return a - b;
+};
+console.log(addNumber(16, 4));
+
+let mulNumber = function (a: number, b: number): number {
+  return a * b;
+};
+console.log(addNumber(16, 4));
+
+let DivNumber = function (a: number, b: number): number {
+  return a / b;
+};
+console.log(addNumber(16, 4));
+
+
+// adding two string
+
+const fullName = function(firstname:"string", lastname:"string"):string{
+  return firstname + lastname
+}
+
+console.log("Aichchhik", "Yadav");
