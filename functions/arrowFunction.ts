ec744bf94f890f0f72743e889faa8bf2bc6eef8b
Ak-Yadav3122 <aichchhik12@gmail.com>
2025-01-31 15:23:12 +0530

    use the arrow function

---
+++ functions/arrowFunction.ts
@@ -0,0 +1,23 @@
+const sum = (a: number, b: number): number => {
+  return a + b;
+};
+
+console.log(sum(4, 5));
+
+const sub = (a: number, b: number): number => {
+  return a - b;
+};
+
+console.log(sum(10, 5));
+
+const mul = (a: number, b: number): number => {
+  return a * b;
+};
+
+console.log(mul(4, 5));
+
+const div = (a: number, b: number): number => {
+  return a / b;
+};
+
+console.log(div(20, 5));
